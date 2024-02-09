a34d51d426bf722aeef1966a6e9ea233467380dd
Georgio Bejjani <georgio.bejjani@moobitek.com>
2024-02-09 11:09:37 +0200

    Implemented upperHeader content

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import classes from "./Header.module.scss";
+import contactUsIcon from "../../assets/contactUsIcon.png";
+
+export default function Header() {
+    return (
+        <header className={classes.header}>
+            <div className="container">
+                <nav className={classes.navBar}>
+                    <span className={classes.navBar__link}>Need help?</span>
+                    <button className={classes.navBar__btnAction}>
+                        <img
+                            alt="contactUs-icon"
+                            src={contactUsIcon}
+                            className={classes.navBar__icon}
+                        />
+                        contact us
+                    </button>
+                    <div className={classes.closeContainer} />
+                </nav>
+            </div>
+        </header>
+    );
+}
